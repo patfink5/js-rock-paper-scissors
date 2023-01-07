da0e72cedb77bf5990434fa901d893c017eaa0dd
Patrick Fink <patfink5@gmail.com>
2023-01-07 18:21:04 -0500

    add function to play a round of rps

--- rps-game.js
+++ rps-game.js
@@ -8,24 +8,35 @@
 const rps = ["rock", "paper", "scissors"];
 
 //Computers Choice
-let pc_selection = (function getComputerChoice() {
+function getComputerChoice() {
 
-    let computerChoice = Math.floor(Math.random() * rps.length);
-    let computerSelection = rps[computerChoice];
-    return computerSelection;
-})();
+    let randomizer = Math.floor(Math.random() * rps.length);
+    let computerChoice = rps[randomizer];
+    return computerChoice;
+};
+
+let computerSelection = getComputerChoice();
 
 //Players Choice
 let playerSelection = window.prompt("Choose - rock, paper, scissors:", "rock");
-console.log(playerSelection);
-console.log(pc_selection);
-
-//Alert 
 
 
+//Single Round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+       return `It's a tie! you both picked ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+       return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+       return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+       return "You win! Scissors beats Paper";
+    } else {
+       return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+ }
 
-
-
+ playRound(playerSelection, computerSelection);
 
 //Alert
 // function playerSelection () {
