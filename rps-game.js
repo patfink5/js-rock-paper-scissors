2c55a92cf241fbd28b4cc43c9d5328b5c7784014
Patrick Fink <patfink5@gmail.com>
2023-01-07 11:57:31 -0500

    create prompt for user selection

--- rps-game.js
+++ rps-game.js
@@ -1,11 +1,42 @@
+// Purpose: Script to play RPS in the console
+// Date:    2023-07-01
+
+//Create a function that plays  a single round of rps, by 
+//having the pc print it's choice after the user writes 
+//theirs, and then declares a winner.
+
+const rps = ["rock", "paper", "scissors"];
+
+//Computer Choice
 function getComputerChoice() {
-    const rps = ["rock", "paper", "scissors"];
 
     let computerChoice = Math.floor(Math.random() * rps.length);
-    console.log(computerChoice, rps[computerChoice]);
+    let computerSelection = rps[computerChoice];
+    console.log(computerSelection);
 }
 
-getComputerChoice();
+
+//Player Choice
+function playerSelection () {
+    console.log("Let's play RPS! 1, 2, 3 ... Shoot!");
+    prompt("Choose - rock, paper, scissors:", "rock");
+}
+
+//Alert
+// function playerSelection () {
+//     console.log("Let's play RPS! 1, 2, 3 ... Shoot!");
+//     prompt("Choose - rock, paper, scissors:", "rock");
+// }
 
 
+//Play Game
+function playRound () {
+    playerSelection ();
+    getComputerChoice();
+    console.log(computerSelection);
+
+
+}
+
+playRound();
 
